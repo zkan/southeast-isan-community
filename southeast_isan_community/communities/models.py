7b0d75f1fae9dcb2979978229988903fe6b1085b
zkan <kan@prontomarketing.com>
2019-01-06 13:44:07 +0700

    Set plural name for community :bear:

--- southeast_isan_community/communities/models.py
+++ southeast_isan_community/communities/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Community(models.Model):
     name = models.CharField(null=False, blank=False, max_length=150)
+
+    class Meta:
+        verbose_name_plural = 'communities'
--- southeast_isan_community/communities/tests/test_models.py
+++ southeast_isan_community/communities/tests/test_models.py
@@ -4,12 +4,19 @@ from ..models import Community
 
 
 class CommunityTest(TestCase):
+    def setUp(self):
+        self.community = Community()
+
     def test_save_community(self):
-        community = Community()
-        community.name = 'ชุมชนบ้านเชื้อเพลิง (Chueaploeng Community)'
-        community.save()
+        expected = 'ชุมชนบ้านเชื้อเพลิง (Chueaploeng Community)'
+        self.community.name = expected
+        self.community.save()
 
         community = Community.objects.last()
-
-        expected = 'ชุมชนบ้านเชื้อเพลิง (Chueaploeng Community)'
         self.assertEqual(community.name, expected)
+
+    def test_community_should_show_correct_plural_name(self):
+        self.assertEqual(
+            self.community._meta.verbose_name_plural,
+            'communities'
+        )
