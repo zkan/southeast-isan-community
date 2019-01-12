2bba55f342e3505f0df17f131f5ac03b88d4e994
zkan <kan@prontomarketing.com>
2019-01-12 10:16:46 +0700

    Make other fields except name not required :bear:

--- southeast_isan_community/communities/models.py
+++ southeast_isan_community/communities/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Community(models.Model):
     name = models.CharField(null=False, blank=False, max_length=150)
     regional_dialect = models.CharField(
-        null=False,
-        blank=False,
+        null=True,
+        blank=True,
         max_length=150
     )
-    faith = models.CharField(null=False, blank=False, max_length=150)
-    folk_game = models.CharField(null=False, blank=False, max_length=150)
+    faith = models.CharField(null=True, blank=True, max_length=150)
+    folk_game = models.CharField(null=True, blank=True, max_length=150)
 
     class Meta:
         verbose_name_plural = 'communities'
