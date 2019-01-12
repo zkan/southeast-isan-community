d72ce5b5870e4a95f308eef3662d3d943adcd9ac
zkan <kan@prontomarketing.com>
2019-01-12 10:14:46 +0700

    Add culture-related fields to community :bear:

--- southeast_isan_community/communities/models.py
+++ southeast_isan_community/communities/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 class Community(models.Model):
     name = models.CharField(null=False, blank=False, max_length=150)
+    regional_dialect = models.CharField(
+        null=False,
+        blank=False,
+        max_length=150
+    )
+    faith = models.CharField(null=False, blank=False, max_length=150)
+    folk_game = models.CharField(null=False, blank=False, max_length=150)
 
     class Meta:
         verbose_name_plural = 'communities'
--- southeast_isan_community/communities/tests/test_models.py
+++ southeast_isan_community/communities/tests/test_models.py
@@ -6,13 +6,25 @@ from ..models import Community
 class CommunityTest(TestCase):
     def setUp(self):
         self.community_name = 'ชุมชนบ้านเชื้อเพลิง (Chueaploeng Community)'
+        self.community_regional_dialect = 'โอะโยะ'
+        self.community_faith = 'ภูตผี'
+        self.community_folk_game = 'มอญซ่อนผ้า'
         self.community = Community.objects.create(
-            name=self.community_name
+            name=self.community_name,
+            regional_dialect=self.community_regional_dialect,
+            faith=self.community_faith,
+            folk_game=self.community_folk_game
         )
 
     def test_community_should_be_persisted_correctly(self):
         community = Community.objects.last()
         self.assertEqual(community.name, self.community_name)
+        self.assertEqual(
+            community.regional_dialect,
+            self.community_regional_dialect
+        )
+        self.assertEqual(community.faith, self.community_faith)
+        self.assertEqual(community.folk_game, self.community_folk_game)
 
     def test_community_should_show_correct_plural_name(self):
         self.assertEqual(
